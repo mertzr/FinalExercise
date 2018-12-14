85d8cb2a9c923949c8d33bacca9001d0fdd7e52e
ryanm <ryanm@DESKTOP-8LTUF54>
2018-12-13 20:53:34 -0500

    FINAL

--- FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
@@ -52,7 +52,30 @@ public class RetirementController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {		
 	}
 	
-	
+	private boolean isInt(String txtYearsToWork, String txtYearsRetired ){
+		try {
+			Integer.parseInt(txtYearsToWork);
+			Integer.parseInt(txtYearsRetired);
+			
+			return true;
+		}
+		catch(NumberFormatException e) {
+			return false;
+		}
+	}
+	private boolean isDouble(String txtAnnualReturnWork, String txtAnnualReturnRetired, String txtRequiredIncome, String txtMonthlySSI ){
+		try {
+			Double.parseDouble(txtAnnualReturnWork);
+			Double.parseDouble(txtAnnualReturnRetired);
+			Double.parseDouble(txtRequiredIncome);
+			Double.parseDouble(txtMonthlySSI);
+			
+			return true;
+		}
+		catch(NumberFormatException e) {
+			return false;
+		}
+	}
 	@FXML
 	public void btnClear(ActionEvent event) {
 		txtYearsToWork.setText("");
@@ -72,7 +95,7 @@ public class RetirementController implements Initializable {
 		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
 		
 		Retirement rt = new Retirement();
-		
+		if(isInt(txtYearsToWork.getText(),txtYearsRetired.getText())==true&& isDouble(txtAnnualReturnWork.getText(), txtAnnualReturnRetired.getText(),txtRequiredIncome.getText(), txtMonthlySSI.getText())==true) {
 		
 		rt.setiYearsToWork(Integer.parseInt(txtYearsToWork.getText()));
 		rt.setdAnnualReturnWorking(Double.parseDouble(txtAnnualReturnWork.getText()));
@@ -81,13 +104,19 @@ public class RetirementController implements Initializable {
 		rt.setdRequiredIncome(Double.parseDouble(txtRequiredIncome.getText()));
 		rt.setdMonthlySSI(Double.parseDouble(txtMonthlySSI.getText()));
 		
-		
-		
 		labelWhatYouNeed.setText(String.valueOf(rt.TotalAmountSaved()));
 		labelSaveEachMonth.setText(String.valueOf(rt.AmountToSave()));
+		}
 		
-	
-		
-		
+		else {
+			txtYearsToWork.setText("");
+			txtAnnualReturnWork.setText("");
+			txtYearsRetired.setText("");
+			txtAnnualReturnRetired.setText("");
+			txtRequiredIncome.setText("");
+			txtMonthlySSI.setText("");
+			labelSaveEachMonth.setText("Please Enter Valid Entries");
+			labelWhatYouNeed.setText("Please Enter Valid Entries");
+		}
 	}
 }
